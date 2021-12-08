3f288eec0f8179067a63b8afd53d84975a60b5ec
Nandhini Venkatesan <x20236158@student.ncirl.ie>
2021-12-08 12:19:38 +0000

    added wallet.js

--- wallet.js
+++ wallet.js
@@ -23,7 +23,7 @@ const getWalletDetails=async()=>{
      console.log(`eth address is 0x${ethAddress}`);
 
 }
-getWalletDetails();
+//getWalletDetails();
 
 const bip39 = require('bip39');
 const { hdkey } = require('ethereumjs-wallet');
@@ -35,7 +35,30 @@ const { hdkey } = require('ethereumjs-wallet');
 const getHDWalletDetails = async() => {
     console.log("getting hd wallet details...")
 
+    // takes a seed phrase (mnemonic)
     const mnemonic = "secrit seed phrase goes here"
-
+ 
+    // converts the seed phrase into entropy
     let seed = await bip39.mnemonicToSeed(mnemonic);
+
+    let hdwallet = hdkey.fromMasterSeed(seed);
+
+    // takes in a 'path' to create a deterministic account
+    const path = "m/44'/60'/0'/0/0";
+    const wallet = hdwallet.derivePath(path).getWallet();
+    console.log(`wallet is ${wallet}`);
+
+    console.log("wallet public key:" + wallet.getPublicKeyString());
+
+    //hash the public key (keccak256)
+    const pubKey =  wallet.getPublicKey();
+    const hashedPublicKey = keccak256(pubKey).toString('hex');
+    console.log(`hashed public key: 0x${hashedPublicKey}`);
+
+     // - drop the first 12 bytes
+     var ethAddress = hashedPublicKey.substring(24);
+     console.log(`eth address is 0x${ethAddress}`);
+
 }
+
+getHDWalletDetails();
